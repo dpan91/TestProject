438cd50a19533f3ebc1a001f3a2f9b3569c0042f
dipan <dipan@Dipan.home>
2025-03-28 14:51:40 -0400

    Updated

---
+++ MyFirstJavaApp/src/Java/Variable.java
@@ -0,0 +1,15 @@
+package Java;
+
+public class Variable {
+	public static void main(String[] args) {
+	       // Variable = a reusable container for a value
+		   //          = a variable behaves as if it was the value it contains
+			/*
+			  Primitive = simple value stored directly in memory(stack)
+			  Reference = memory address(stack) that point to the (heap)
+			 */
+		
+	       System.out.println("Hello World");
+	       System.out.println("I Love Java");
+		}
+}
