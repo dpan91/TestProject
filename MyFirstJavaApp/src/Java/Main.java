995cf9336c0f6338c82ce5c7482533f90d0a7b52
dipan <dipan@Dipan.home>
2025-03-28 14:32:05 -0400

    commit

---
+++ MyFirstJavaApp/src/Java/Main.java
@@ -0,0 +1,13 @@
+package Java;
+
+public class Main {
+
+	public static void main(String[] args) {
+       // This my first java program : Single line comment
+		/*
+		  This is multi-line comment
+		 */
+       System.out.println("Hello World");
+	}
+
+}
