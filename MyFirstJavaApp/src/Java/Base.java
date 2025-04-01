8dc5b07a5e1d11a82e77122978206cb8fb2e0cde
dipan <dipan@Dipan.home>
2025-04-01 17:19:19 -0400

    Base Code

---
+++ MyFirstJavaApp/src/Java/Base.java
@@ -0,0 +1,33 @@
+package Java;
+
+public class Base{
+	public static void main(String[] args) {
+	       // Variable = a reusable container for a value
+		   //          = a variable behaves as if it was the value it contains
+			/*
+			  Primitive = simple value stored directly in memory(stack)
+			  Reference = memory address(stack) that point to the (heap)
+			 */
+		boolean flag = true;
+		char ch = 'A';
+		byte b = 12;
+		short s = 24;
+		int i = 257;
+		long l = 890L;
+		float f = 3.145F;
+		double d = 2.1828;
+		   
+		   
+	       System.out.println("Hello World");
+	       System.out.println("I Love Java");
+	       System.out.println("flag =" +flag);
+	       System.out.println("ch =" +ch);
+	       System.out.println("b =" +b);
+	       System.out.println("s =" +s);
+	       System.out.println("i =" +i);
+	       System.out.println("l =" +l);
+	       System.out.println("f =" +f);
+	       System.out.println("d =" +d);
+	       
+		}
+}
